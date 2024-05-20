8b9fdc93a397e65d75ddb525a3dd6aea2c5dda06
Giovani de Oliveira <giovani98@proton.me>
2024-05-20 15:00:52 -0300

    feat: Add css and modal

--- src/App.js
+++ src/App.js
@@ -6,11 +6,29 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 export function App() {
-  const baseUrl = 'https://localhost:44377/api/alunos'
-
   const [data, setData] = useState([])
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [alunoSelecionado, setAlunoSelecionado] = useState({
+    id: '',
+    nome: '',
+    email: '',
+    idade: '',
+  })
 
-  const getAlunos = async () => {
+  useEffect(() => {
+    getAlunos()
+  })
+
+  function openModal() {
+    setIsModalOpen(true)
+  }
+
+  function closeModal() {
+    setIsModalOpen(false)
+  }
+
+  const baseUrl = 'https://localhost:44377/api/alunos'
+  async function getAlunos() {
     await axios
       .get(baseUrl)
       .then((response) => {
@@ -21,17 +39,39 @@ export function App() {
       })
   }
 
-  useEffect(() => {
-    getAlunos()
-  })
+  function handleChange(e) {
+    const { name, value } = e.target
+    setAlunoSelecionado({
+      ...alunoSelecionado,
+      [name]: value,
+    })
+    console.log(alunoSelecionado)
+  }
+
+  async function postAlunos() {
+    delete alunoSelecionado.id
+
+    alunoSelecionado.idade = parseInt(alunoSelecionado.idade)
+    await axios
+      .post(baseUrl, alunoSelecionado)
+      .then((response) => {
+        setData(data.concat(response.data))
+        closeModal()
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
 
   return (
-    <div className="App">
+    <div className="aluno-container">
       <br />
       <h3>Cadastro de alunos</h3>
       <header>
         <img src={logoCadastro} alt="Cadastro" />
-        <button className="btn btn-success">Incluir aluno</button>
+        <button className="btn btn-success" onClick={openModal}>
+          Incluir aluno
+        </button>
       </header>
       <table className="table table-bordered">
         <thead>
@@ -60,6 +100,46 @@ export function App() {
           ))}
         </tbody>
       </table>
+
+      <Modal isOpen={isModalOpen}>
+        <ModalHeader>Incluir alunos</ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>Nome: </label>
+            <br />
+            <input
+              type="text"
+              className="form-control"
+              name="nome"
+              onChange={handleChange}
+            />
+            <label>Email: </label>
+            <br />
+            <input
+              type="text"
+              className="form-control"
+              name="email"
+              onChange={handleChange}
+            />
+            <label>Idade: </label>
+            <br />
+            <input
+              type="text"
+              className="form-control"
+              name="idade"
+              onChange={handleChange}
+            />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={postAlunos}>
+            Incluir
+          </button>
+          <button className="btn btn-neutral" onClick={closeModal}>
+            Cancelar
+          </button>
+        </ModalFooter>
+      </Modal>
     </div>
   )
 }
